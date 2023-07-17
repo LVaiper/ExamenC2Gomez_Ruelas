f0ed9fc197503fdc2f40ea951b02911ba0d7a0e5
IVAN ENRIQUE GOMEZ RUELAS <2020030199@upsin.edu.mx>
2023-07-17 13:53:12 -0700

    Desarrollo de la clase BombaGasolina

---
+++ app/src/main/java/com/example/examencorte02/BombaGasolina.java
@@ -0,0 +1,13 @@
+package com.example.examencorte02;
+
+public class BombaGasolina {
+
+    private int id;
+    private int numBomba;
+    private int capacidadBomba;
+    private int tipoGasolina;
+    private double precio;
+    private int acumuladoLitrosBomba;
+
+
+}
